11be74d39fc4a76af1c2f933edc4767281a64d05
Jewel Uzquiano <uzquianojewel@gmail.com>
2021-10-10 17:13:18 -0500

    Update about.js

--- about.js
+++ about.js
@@ -14,7 +14,7 @@ var config = {
             title: 'Philis M. Barragán Goetz',
             image: 'images/philis-barragan-goetz-headshot.jpg',
             //image: '/staticmap.html',
-            description: '',
+            description: 'Here is where we can have a short description about our backgrounds.',
             location: {
               center: [-98.57101, 29.43056],
               zoom: 9.42,
